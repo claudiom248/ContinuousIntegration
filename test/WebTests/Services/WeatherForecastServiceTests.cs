8190aa7fa31cf1c3ec705ad676af7dd1fe72f15f
Claudio Manniti <claudio.manniti@outlook.com>
2022-12-13 23:39:08 +0100

    Raise coverage

--- test/WebTests/Services/WeatherForecastServiceTests.cs
+++ test/WebTests/Services/WeatherForecastServiceTests.cs
@@ -12,19 +12,19 @@ namespace WebTests.Services
             Assert.NotEmpty(weatherForecast);
         }
 
-        //[Fact]
-        //public void UncoveredMethod_Throws()
-        //{
-        //    var sut = new WeatherForecastService();
-        //    Assert.Throws<NotImplementedException>(sut.UncoveredMethod);
-        //}
+        [Fact]
+        public void UncoveredMethod_Throws()
+        {
+            var sut = new WeatherForecastService();
+            Assert.Throws<NotImplementedException>(sut.UncoveredMethod);
+        }
 
         [Theory]
         [InlineData(true, 1, 1)]
         [InlineData(true, 2, 2)]
-        //[InlineData(true, 3, 3)]
-        //[InlineData(false, 1, 0)]
-        //[InlineData(false, 2, 1)]
+        [InlineData(true, 3, 3)]
+        [InlineData(false, 1, 0)]
+        [InlineData(false, 2, 1)]
         public void PartiallyCovedereMethod_Branch1(bool flag, byte param, int expected)
         {
             var sut = new WeatherForecastService();
