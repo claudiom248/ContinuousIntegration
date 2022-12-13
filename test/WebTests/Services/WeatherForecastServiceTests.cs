1a4f0e6b4630b3622590160647baba899021b624
Claudio Manniti <claudio.manniti@outlook.com>
2022-12-13 21:18:57 +0100

    Comment another test case

--- test/WebTests/Services/WeatherForecastServiceTests.cs
+++ test/WebTests/Services/WeatherForecastServiceTests.cs
@@ -23,7 +23,7 @@ namespace WebTests.Services
         [InlineData(true, 1, 1)]
         [InlineData(true, 2, 2)]
         [InlineData(true, 3, 3)]
-        [InlineData(false, 1, 0)]
+        //[InlineData(false, 1, 0)]
         //[InlineData(false, 2, 1)]
         public void PartiallyCovedereMethod_Branch1(bool flag, byte param, int expected)
         {
